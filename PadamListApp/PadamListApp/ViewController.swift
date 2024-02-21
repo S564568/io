727092e9adf1c84ed644d54a2b394ed3935c08ed
teja <s564568@nwmissouri.edu>
2024-02-21 01:38:37 -0600

    normal commit

---
+++ PadamListApp/PadamListApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  PadamListApp
+//
+//  Created by Lakshmi Sai Teja Padam on 2/21/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
